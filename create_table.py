cbc958ebed1928b04290d55c73e15d5a6b57ea01
Юсупов Александр / Iusupov Aleksandr <ariusupov@ekat2023.com>
2022-12-05 17:18:45 +0500

    data2.db

--- create_table.py
+++ create_table.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection  = sqlite3.connect('data.db')
+connection  = sqlite3.connect('data2.db')
 cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
