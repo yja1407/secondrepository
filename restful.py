6fd9e5e28ca0aef010ac6ddf60b93b84b3fa638b
Юсупов Александр / Iusupov Aleksandr <ariusupov@ekat2023.com>
2022-12-06 11:54:44 +0500

    secret key

--- restful.py
+++ restful.py
@@ -10,7 +10,7 @@ from Resources.Items import Item, Itemlist
 from Resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.secret_key = 'jose' # app.config['JWT_SECRET_KEY']
+app.secret_key = 'yja' # app.config['JWT_SECRET_KEY']
 api = Api(app)
 ##app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///C:/Flask_course/section6/data2.db'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data2.db'
